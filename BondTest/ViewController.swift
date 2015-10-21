bb78c66382e49b25b4f37f8db7384c30442dc618
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-10-21 21:52:12 +0900

    なんかついか

--- BondTest/ViewController.swift
+++ BondTest/ViewController.swift
@@ -12,6 +12,10 @@ import Bond
 class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var combineField1: UITextField!
+    @IBOutlet weak var combineField2: UITextField!
+    @IBOutlet weak var combineButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,21 @@ class ViewController: UIViewController {
         
         // テキストフィールドの値をラベルにバインディング
         textField.bnd_text.bindTo(label.bnd_text)
+        
+        // 値に文字列を追加する
+        textField.bnd_text
+            .map { "Hi " + $0! }
+            .bindTo(label.bnd_text)
+        
+        button.bnd_tap.observe { (event) in
+            print("button tapped.")
+        }
+        
+        // ストリームっぽいやつ
+        combineLatest(combineField1.bnd_text, combineField2.bnd_text)
+            .map { com1, com2 in
+            return com1!.characters.count > 4 && com2!.characters.count > 4
+        }.bindTo(combineButton.bnd_enabled)
     }
 
     override func didReceiveMemoryWarning() {
