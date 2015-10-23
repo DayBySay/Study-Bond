d981382b585cebd3914e89b518f3b96476a30f04
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-10-23 13:01:59 +0900

    データソース用の配列追加

--- BondTest/Article.swift
+++ BondTest/Article.swift
@@ -11,4 +11,11 @@ import Bond
 
 struct Article {
     var title: Observable<String?>
+    let all: [Article] = [
+            Article(title: Observable("タイトル1")),
+            Article(title: Observable("タイトル2")),
+            Article(title: Observable("タイトル3")),
+            Article(title: Observable("タイトル4")),
+            Article(title: Observable("タイトル5")),
+        ]
 }
